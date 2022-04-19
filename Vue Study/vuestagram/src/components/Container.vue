f1ce5ffbfe2e7de64b15610578725051620bee7b
Gucciheon <mintwkd@naver.com>
2022-04-19 17:11:50 +0900

    .

--- Vue Study/vuestagram/src/components/Container.vue
+++ Vue Study/vuestagram/src/components/Container.vue
@@ -1,6 +1,27 @@
 <template>
-  <div>
-    <Post :Data="Data[index]" v-for="(a,index) in Data" :key="index"/>
+  <!-- 저번시간에 만든거 -->
+  <div v-if="step == 0">
+    <Post :Data="Data[i]" v-for="(a, i) in Data" :key="i" />
+  </div>
+
+  <!-- 필터선택페이지 -->
+  <div v-if="step == 1">
+    <div class="upload-image" :style="`background-image:url(${image})`"></div>
+    <div class="filters">
+      <div class="filter-1"></div>
+      <div class="filter-1"></div>
+      <div class="filter-1"></div>
+      <div class="filter-1"></div>
+      <div class="filter-1"></div>
+    </div>
+  </div>
+
+  <!-- 글작성페이지 -->
+  <div v-if="step == 2">
+    <div class="upload-image"></div>
+    <div class="write">
+      <textarea class="write-box">write!</textarea>
+    </div>
   </div>
 </template>
 
@@ -12,10 +33,54 @@ export default {
     Post,
   },
   props: {
-    Data:Array,
+    Data: Array,
+    step: Number,
+    image: String,
   },
 };
 </script>
 
 <style>
+.upload-image {
+  width: 100%;
+  height: 450px;
+  background: cornflowerblue;
+  background-size: cover;
+}
+.filters {
+  overflow-x: scroll;
+  white-space: nowrap;
+}
+.filter-1 {
+  width: 100px;
+  height: 100px;
+  background-color: cornflowerblue;
+  margin: 10px 10px 10px auto;
+  padding: 8px;
+  display: inline-block;
+  color: white;
+  background-size: cover;
+}
+.filters::-webkit-scrollbar {
+  height: 5px;
+}
+.filters::-webkit-scrollbar-track {
+  background: #f1f1f1;
+}
+.filters::-webkit-scrollbar-thumb {
+  background: #888;
+  border-radius: 5px;
+}
+.filters::-webkit-scrollbar-thumb:hover {
+  background: #555;
+}
+.write-box {
+  border: none;
+  width: 90%;
+  height: 100px;
+  padding: 15px;
+  margin: auto;
+  display: block;
+  outline: none;
+}
 </style>
